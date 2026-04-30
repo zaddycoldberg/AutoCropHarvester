6e6864670dadaf9b816a87bda504ad35775d4ef3
zaddycoldberg <zaddycoldberg@gmail.com>
2026-04-30 22:57:06 +0530

    Create TickHandler.java

---
+++ src/main/java/com/autoharvester/handler/TickHandler.java
@@ -0,0 +1,17 @@
+package com.autoharvester.handler;
+
+import com.autoharvester.core.SnakeEngine;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
+import net.minecraft.client.Minecraft;
+
+public class TickHandler {
+
+    @SubscribeEvent
+    public void onClientTick(TickEvent.ClientTickEvent event) {
+        if (event.phase != TickEvent.Phase.START) return;
+        Minecraft mc = Minecraft.getMinecraft();
+        if (mc.thePlayer == null) return;
+        SnakeEngine.get().tick();
+    }
+}
