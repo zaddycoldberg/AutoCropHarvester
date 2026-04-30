4b83c8bbdcd29934c99574aa84dbf3f73b6f1cf9
zaddycoldberg <zaddycoldberg@gmail.com>
2026-04-30 17:31:14 +0530

    Update KeyBindings.java

--- src/main/java/com/autoharvester/keybind/KeyBindings.java
+++ src/main/java/com/autoharvester/keybind/KeyBindings.java
@@ -1,7 +1,7 @@
 package com.autoharvester.keybind;
 
 import net.minecraft.client.settings.KeyBinding;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.client.registry.ClientRegistry;
 import org.lwjgl.input.Keyboard;
 
 public class KeyBindings {
