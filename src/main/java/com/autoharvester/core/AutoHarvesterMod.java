6f14e8b6646c859bc7981f7c3f2984aa97112b55
zaddycoldberg <zaddycoldberg@gmail.com>
2026-04-30 17:32:08 +0530

    Update AutoHarvesterMod.java

--- src/main/java/com/autoharvester/core/AutoHarvesterMod.java
+++ src/main/java/com/autoharvester/core/AutoHarvesterMod.java
@@ -8,35 +8,20 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.common.MinecraftForge;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-@Mod(
-    modid = AutoHarvesterMod.MODID,
-    name = AutoHarvesterMod.NAME,
-    version = AutoHarvesterMod.VERSION,
-    acceptedMinecraftVersions = "[1.8.9]"
-)
+@Mod(modid = "autoharvester", name = "Auto Crop Harvester", version = "1.0.0", acceptedMinecraftVersions = "[1.8.9]")
 public class AutoHarvesterMod {
 
-    public static final String MODID = "autoharvester";
-    public static final String NAME = "Auto Crop Harvester";
-    public static final String VERSION = "1.0.0";
-
-    public static final Logger LOGGER = LogManager.getLogger(MODID);
-
-    @Mod.Instance(MODID)
+    @Mod.Instance("autoharvester")
     public static AutoHarvesterMod instance;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        LOGGER.info("AutoHarvester PreInit...");
         KeyBindings.register();
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        LOGGER.info("AutoHarvester Init...");
         MinecraftForge.EVENT_BUS.register(new TickHandler());
         MinecraftForge.EVENT_BUS.register(new KeyInputHandler());
     }
