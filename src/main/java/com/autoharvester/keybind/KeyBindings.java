33ad129531f49c78de31f9277b42c721368b1298
zaddycoldberg <zaddycoldberg@gmail.com>
2026-04-30 15:10:31 +0530

    Update KeyBindings.java

--- src/main/java/com/autoharvester/keybind/KeyBindings.java
+++ src/main/java/com/autoharvester/keybind/KeyBindings.java
@@ -1,7 +1,7 @@
 package com.autoharvester.keybind;
 
 import net.minecraft.client.settings.KeyBinding;
-import cpw.mods.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import org.lwjgl.input.Keyboard;
 
 public class KeyBindings {
@@ -11,8 +11,8 @@ public class KeyBindings {
     public static KeyBinding KEY_LOCK_ANGLE;
 
     public static void register() {
-        KEY_TOGGLE     = new KeyBinding("key.autoharvester.toggle",    Keyboard.KEY_H, "key.category.autoharvester");
-        KEY_GUI        = new KeyBinding("key.autoharvester.gui",       Keyboard.KEY_G, "key.category.autoharvester");
+        KEY_TOGGLE = new KeyBinding("key.autoharvester.toggle", Keyboard.KEY_H, "key.category.autoharvester");
+        KEY_GUI = new KeyBinding("key.autoharvester.gui", Keyboard.KEY_G, "key.category.autoharvester");
         KEY_LOCK_ANGLE = new KeyBinding("key.autoharvester.lockangle", Keyboard.KEY_L, "key.category.autoharvester");
 
         ClientRegistry.registerKeyBinding(KEY_TOGGLE);
