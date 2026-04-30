3877f09837a822cce94d175c27c25a4567382c5c
zaddycoldberg <zaddycoldberg@gmail.com>
2026-04-30 17:43:07 +0530

    Update KeyBindings.java

--- src/main/java/com/autoharvester/keybind/KeyBindings.java
+++ src/main/java/com/autoharvester/keybind/KeyBindings.java
@@ -1,6 +1,7 @@
 package com.autoharvester.keybind;
 
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.client.ClientCommandHandler;
 import cpw.mods.fml.client.registry.ClientRegistry;
 import org.lwjgl.input.Keyboard;
 
@@ -11,9 +12,21 @@ public class KeyBindings {
     public static KeyBinding KEY_LOCK_ANGLE;
 
     public static void register() {
-        KEY_TOGGLE = new KeyBinding("key.autoharvester.toggle", Keyboard.KEY_H, "key.category.autoharvester");
-        KEY_GUI = new KeyBinding("key.autoharvester.gui", Keyboard.KEY_G, "key.category.autoharvester");
-        KEY_LOCK_ANGLE = new KeyBinding("key.autoharvester.lockangle", Keyboard.KEY_L, "key.category.autoharvester");
+        KEY_TOGGLE = new KeyBinding(
+            "key.autoharvester.toggle",
+            Keyboard.KEY_H,
+            "key.category.autoharvester"
+        );
+        KEY_GUI = new KeyBinding(
+            "key.autoharvester.gui",
+            Keyboard.KEY_G,
+            "key.category.autoharvester"
+        );
+        KEY_LOCK_ANGLE = new KeyBinding(
+            "key.autoharvester.lockangle",
+            Keyboard.KEY_L,
+            "key.category.autoharvester"
+        );
 
         ClientRegistry.registerKeyBinding(KEY_TOGGLE);
         ClientRegistry.registerKeyBinding(KEY_GUI);
