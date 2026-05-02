35366abead35269853d88e181c5e21e1cbda0e4a
zaddycoldberg <zaddycoldberg@gmail.com>
2026-05-02 10:55:23 +0530

    fix: update FML imports to net.minecraftforge.fml (Forge 1.8.9)

--- src/main/java/com/autoharvester/core/AutoHarvesterMod.java
+++ src/main/java/com/autoharvester/core/AutoHarvesterMod.java
@@ -3,10 +3,10 @@ package com.autoharvester.core;
 import com.autoharvester.handler.KeyInputHandler;
 import com.autoharvester.handler.TickHandler;
 import com.autoharvester.keybind.KeyBindings;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.common.MinecraftForge;
 
 @Mod(modid = "autoharvester", name = "Auto Crop Harvester", version = "1.0.0", acceptedMinecraftVersions = "[1.8.9]")
